b4cfec0712ad558f3baa2a33dc18f87d2f5bdbc0
mrefky <mrefky@hotmail.com>
2024-05-24 10:59:46 +0300

    1

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
-microk8s kubectl delete -f /home/mrefky/Thunder/InboudGW/InboundGW.yaml
+microk8s kubectl delete -f ./InboundGW.yaml
 ./delete_kafka.sh
 ./install_kafka.sh
 ./reset0.sh
 go build .
-/home/mrefky/Thunder/InboudGW/create_inboundgw.sh
-docker build -t localhost:32000/inboundgw  /home/mrefky/Thunder/InboudGW/.
+./create_inboundgw.sh
+docker build -t localhost:32000/inboundgw  .
 docker push localhost:32000/inboundgw
 sleep 1
-microk8s kubectl create -f /home/mrefky/Thunder/InboudGW/InboundGW.yaml
+microk8s kubectl create -f ./InboundGW.yaml
 microk8s kubectl wait pods inboundgw-0  --for condition=Ready --timeout=90s
-/home/mrefky/Thunder/InboudGW/expose.sh
+./expose.sh
--- create_inboundgw.sh
+++ create_inboundgw.sh
@@ -1,3 +1,3 @@
 microk8s.kubectl delete configmap inboundgw-config
-microk8s.kubectl create configmap inboundgw-config --from-file=/home/mrefky/Thunder/InboudGW/executor.cfg
+microk8s.kubectl create configmap inboundgw-config --from-file=./executor.cfg
 
