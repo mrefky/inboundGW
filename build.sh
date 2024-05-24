70edcf87694a87fcc4e1616c1736ec8d4745973e
mrefky <mrefky@hotmail.com>
2024-05-24 12:12:43 +0300

    1

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+microk8s kubectl delete sts,pvc,cm,svc,po --all
+microk8s.kubectl wait pod --all --for=condition=Ready  --all-namespaces
 microk8s kubectl delete -f ./InboundGW.yaml
 microk8s.kubectl wait pod --all --for=condition=Ready  --all-namespaces
 ./delete_kafka.sh
