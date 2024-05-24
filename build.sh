921dbba0dadc2df690b57970dd51e77960c53e68
mrefky <mrefky@hotmail.com>
2024-05-24 14:40:51 +0300

    1

--- build.sh
+++ build.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-microk8s kubectl delete sts,pvc,cm,svc,po --all
-microk8s.kubectl wait pod --all --for=condition=Ready  --all-namespaces
+#microk8s kubectl delete sts,pvc,cm,svc,po --all
+#microk8s.kubectl wait pod --all --for=condition=Ready  --all-namespaces
+#./delete_kafka.sh
+#microk8s.kubectl wait pod --all --for=condition=Ready  --all-namespaces
+#./install_kafka.sh
+#microk8s.kubectl wait pod --all --for=condition=Ready  --all-namespaces
+#./reset0.sh
 microk8s kubectl delete -f ./InboundGW.yaml
-microk8s.kubectl wait pod --all --for=condition=Ready  --all-namespaces
-./delete_kafka.sh
-microk8s.kubectl wait pod --all --for=condition=Ready  --all-namespaces
-./install_kafka.sh
-microk8s.kubectl wait pod --all --for=condition=Ready  --all-namespaces
-./reset0.sh
+microk8s.kubectl wait pod --all --for=condition=Ready
 go build .
 ./create_inboundgw.sh
-docker build -t localhost:32000/inboundgw  .
-docker push localhost:32000/inboundgw
+sudo docker build -t localhost:32000/inboundgw  .
+sudo docker push localhost:32000/inboundgw
 sleep 1
 microk8s kubectl create -f ./InboundGW.yaml
 microk8s.kubectl wait pod --all --for=condition=Ready  --all-namespaces
 ./expose.sh
+sleep 1
+microk8s.kubectl logs -f inboundgw-0
--- inboundgw.go
+++ inboundgw.go
@@ -1,40 +1,43 @@
 package main
 
 import (
-	_ "encoding/json"
+	"strconv"
+
 	"flag"
 	"fmt"
-	"log"
-	"os"
-	"os/signal"
 	"path"
 	_ "strings"
 	"syscall"
 	"time"
-
-	"github.com/IBM/sarama"
 	"github.com/quickfixgo/field"
-	fix44er "github.com/quickfixgo/fix44/reject"
 	"github.com/quickfixgo/quickfix"
 	"github.com/quickfixgo/tag"
+	fix44er "github.com/quickfixgo/fix44/reject"
+	_ "encoding/json"
+	"log"
+	"os"
+	"os/signal"
+	"github.com/IBM/sarama"
 )
-
-var last time.Time = time.Now()
-var current time.Time = time.Now()
-var b time.Duration = 0 * time.Millisecond
+var last time.Time  = time.Now()
+var current time.Time  = time.Now()
+var b time.Duration=0 * time.Millisecond
+var ime int=1
 var producer = createProducer()
-
 type executor struct {
 	*quickfix.MessageRouter
 }
 
-func Reject(msg *quickfix.Message, sis quickfix.SessionID) {
-	seq, _ := msg.Header.GetInt(tag.MsgSeqNum)
-	Ric := fix44er.New(field.NewRefSeqNum(seq))
-	// fmt.Println("Rejected")
-	Ric.SetText("Message Rate Exceeded")
-	quickfix.SendToTarget(Ric, sis)
+func Reject(msg *quickfix.Message, sis quickfix.SessionID){
+seq,_:=msg.Header.GetInt(tag.MsgSeqNum)
+Ric:= fix44er.New(field.NewRefSeqNum(seq))
+//fmt.Println("Rejected")
+Ric.SetText("Message Rate Exceeded")
+quickfix.SendToTarget(Ric,sis)
 }
+
+
+
 
 func newExecutor() *executor {
 	e := &executor{MessageRouter: quickfix.NewMessageRouter()}
@@ -42,48 +45,66 @@ func newExecutor() *executor {
 	return e
 }
 
-func (e executor) OnCreate(sessionID quickfix.SessionID)                           {}
-func (e executor) OnLogon(sessionID quickfix.SessionID)                            {}
-func (e executor) OnLogout(sessionID quickfix.SessionID)                           {}
+
+
+
+
+
+func (e executor) OnCreate(sessionID quickfix.SessionID)                           {
+log.Println(" Session ",sessionID," Created")	
+}
+func (e executor) OnLogon(sessionID quickfix.SessionID)                            {
+	log.Println("Session ",sessionID," Logged on")
+ime=1
+}
+func (e executor) OnLogout(sessionID quickfix.SessionID)                           {
+	log.Println("Session ",sessionID," Logged out")
+	ime=1
+}
+
 func (e executor) ToAdmin(msg *quickfix.Message, sessionID quickfix.SessionID)     {}
 func (e executor) ToApp(msg *quickfix.Message, sessionID quickfix.SessionID) error { return nil }
-func (e executor) FromAdmin(msg *quickfix.Message, sessionID quickfix.SessionID) quickfix.MessageRejectError {
-	return nil
-}
+func (e executor) FromAdmin(msg *quickfix.Message, sessionID quickfix.SessionID) quickfix.MessageRejectError {return nil}
 func (e *executor) FromApp(msg *quickfix.Message, sessionID quickfix.SessionID) (reject quickfix.MessageRejectError) {
+//quickfix.SendToTarget(msg,sessionID)
+current=time.Now()
+d:=current.Sub(last)
+//fmt.Println("--------------------")
+//fmt.Println(current,last)
+//fmt.Println(d,b)
+//fmt.Println("--------------------")
 
-	current = time.Now()
-	d := current.Sub(last)
-	//fmt.Println("--------------------")
-	//fmt.Println(current,last)
+if d<=b {
 	//fmt.Println(d,b)
-	//fmt.Println("--------------------")
+	//fmt.Println(d,b," Too Fast Rejected...")
+	Reject(msg,sessionID)
+	return
+	}else{
+		type1,_ :=msg.Header.GetString(35) 
+	log.Println(strconv.Itoa(ime),"Message of type \"",type1," \" Pushed to Inbound topic")
+	ime=ime+1
+	last=time.Now()
+}
+//fmt.Println("--------------------")
+//fmt.Println("")
+last=time.Now()
 
-	if d <= b {
-		//fmt.Println(d,b)
-		//fmt.Println(d,b," Too Fast Rejected...")
-		Reject(msg, sessionID)
-		return
-	} else {
-		ty,_:=msg.MsgType()
-		log.Println(">>>>",ty)
-		log.Println("Pushed to Inbound topic")
-		last = time.Now()
-	}
-	// fmt.Println("--------------------")
-	// fmt.Println("")
-	last = time.Now()
-
-	m4 := msg.String()
-	tag := quickfix.Tag(tag.Symbol)
-	sym, _ := msg.Body.GetString(tag)
-	producer.Input() <- &sarama.ProducerMessage{
+m4:=msg.String()
+tag:=quickfix.Tag(tag.SenderCompID)
+Firm,_:=msg.Header.GetString(tag)
+producer.Input() <- &sarama.ProducerMessage{
 		Topic: "inbound",
 		Value: sarama.ByteEncoder(m4),
-		Key:   sarama.ByteEncoder(sym),
+		Key:   sarama.ByteEncoder(Firm),
 	}
-	return
+	return 
 }
+
+//func (m *quickfix.Message) MsgType() (string, quickfix.MessageRejectError) {
+//	return m.Header.GetString(3)
+//}
+
+
 func main() {
 
 	flag.Parse()
@@ -132,9 +153,9 @@ func createProducer() sarama.AsyncProducer {
 	config.Producer.Return.Errors = true             // notify on failed
 	config.Producer.RequiredAcks = sarama.WaitForAll // waits for all insync replicas to commit
 
-	//	producer, err := sarama.NewAsyncProducer([]string{"192.168.1.70:9094"}, config)
+//	producer, err := sarama.NewAsyncProducer([]string{"192.168.1.70:9094"}, config)
 	producer, err := sarama.NewAsyncProducer([]string{"kafka.default.svc.cluster.local:9092"}, config)
-	if err != nil {
+		if err != nil {
 		log.Fatal("Unable to connect producer to kafka server")
 	}
 
